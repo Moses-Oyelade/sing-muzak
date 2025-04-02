bd844fbf68a9f5bd35b3598bbd160881d5a1132b
Moses-Oyelade <oyelademoses@gmail.com>
2025-04-02 21:17:41 +0100

    update fullstack

--- config/jwt.config.ts
+++
@@ -1,5 +0,0 @@
-export const jwtConstants = {
-    secret: process.env.JWT_SECRET || 'your-secret-key',
-    expiresIn: '1h',
-  };
-  
--- src/modules/performances/performances.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PerformancesService } from './performances.service';
-import { PerformancesController } from './performances.controller';
-
-@Module({
-  providers: [PerformancesService],
-  controllers: [PerformancesController]
-})
-export class PerformancesModule {}
