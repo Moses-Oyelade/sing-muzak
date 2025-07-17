35413196377939639a0585fb0fb7bd231e5d4b90
Moses-Oyelade <oyelademoses@gmail.com>
2025-07-17 01:11:38 +0100

    song and suggestion into user Folder

--- frontend/src/app/dashboard/user/songs/page.tsx
+++ frontend/src/app/dashboard/user/songs/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 // import axiosInstance from 'src/utils/axios'
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import SearchSong from "../dashboard/SearchSongs";
+import SearchSong from "../../SearchSongs";
 
 export default function SongsListPage() {
   // const [songs, setSongs] = useState([]);
