78454ecfd0d4a117320e13de1df11edcdef294fb
Moses-Oyelade <oyelademoses@gmail.com>
2025-07-27 18:02:01 +0100

    update home logo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 py-20 gap-16 sm:pb-20 sm:items-center font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 sm:pb-20 sm:items-center font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start brightness-50 border-y-indigo-800 outline-dotted">
         <div>
           <h1 className="font-bold text-4xl m-4 text-blue-600">
