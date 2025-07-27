269b701b814858febd2a93e6601e51c4a390240a
Moses-Oyelade <oyelademoses@gmail.com>
2025-07-27 19:48:07 +0100

    update home layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[10px_1fr_30px] items-center justify-items-center min-h-screen max-h-screen p-4 py-20 gap-16 sm:pb-20 font-[family-name:var(--font-geist-sans)] overflow-hidden">
+    <div className="grid grid-rows-[10px_1fr_40px] items-center justify-items-center min-h-screen max-h-screen p-4 py-20 gap-16 sm:pb-20 font-[family-name:var(--font-geist-sans)] overflow-hidden">
       <main className="flex flex-col gap-[60px] row-start-2 items-center sm:items-start brightness-50">
         <div className=" border-y-indigo-800 outline-dotted">
           <h1 className="font-bold text-4xl m-4 text-blue-600">
@@ -24,7 +24,7 @@ export default function Home() {
         </div>
       </main>
         <h4 className="text-center sm:text-left">More update to come...</h4>
-       <footer className="w-full flex flex-col items-center row-start-3 text-center sm:text-left">
+       <footer className="w-full flex flex-col items-center row-start-3 pb-6 text-center sm:text-left">
         <div>
           <a
             href="/auth/login"
