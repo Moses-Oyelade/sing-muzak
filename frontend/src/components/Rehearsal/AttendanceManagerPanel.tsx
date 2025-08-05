04944a1e040b1ea1a2b2e6bc14cb19759578c0e8
Moses-Oyelade <oyelademoses@gmail.com>
2025-08-05 02:12:20 +0100

    update present aside admin

--- frontend/src/components/Rehearsal/AttendanceManagerPanel.tsx
+++ frontend/src/components/Rehearsal/AttendanceManagerPanel.tsx
@@ -26,7 +26,7 @@ export default function AttendanceManagerPanel({
       try {
         const data = await getRehearsalById(rehearsalId);
       
-        setMarked(data?.attendees?.map((user: UserType) => user.role === 'member' ? user._id : '') || []);
+        setMarked(data?.attendees?.map((user: UserType) => user._id) || []);
       } catch (err) {
         console.error("Failed to fetch attendees:", err);
         toast.error("Could not load existing attendance.");
@@ -37,7 +37,7 @@ export default function AttendanceManagerPanel({
   }, [rehearsalId, attendees]);
 
   const toggleMarked = (id: string) => {
-    if (marked.includes(id)) {
+    if (marked.includes(id) || attendees.map((attendee)=> (attendee.role === 'admin'))) {
       setMarked((prev) => prev.filter((x) => x !== id));
     } else {
       setMarked((prev) => [...prev, id]);
